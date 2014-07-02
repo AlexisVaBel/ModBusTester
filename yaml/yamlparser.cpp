b527a8f4eacc2ed04f9da4f4e190ee2b56c8fae6
AlexisVaBel <AlexisVaBel@gmail.com>
2014-07-02 18:57:03 +0700

    Need to make yaml

---
+++ yaml/yamlparser.cpp
@@ -0,0 +1,6 @@
+#include "yamlparser.h"
+
+YamlParser::YamlParser(QObject *parent) :
+    QObject(parent)
+{
+}
---
+++ yaml/yamlparser.h
@@ -0,0 +1,18 @@
+#ifndef YAMLPARSER_H
+#define YAMLPARSER_H
+
+#include <QObject>
+
+class YamlParser : public QObject
+{
+    Q_OBJECT
+public:
+    explicit YamlParser(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // YAMLPARSER_H
